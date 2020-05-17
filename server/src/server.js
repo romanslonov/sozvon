33c069ea63e21c9b849144d95280d86fad112271
Roman <destabiliseus@gmail.com>
2020-05-17 18:48:43 +0800

    updated server

--- server/src/server.js
+++ server/src/server.js
@@ -13,48 +13,11 @@ const config = {
   max: 50,
 };
 
-// const {
-//   rooms,
-//   addSocketToRoom,
-//   removeSocketFromRoom,
-//   allSocketsForRoom,
-// } = require('./rooms');
-
 app.use(helmet());
 app.use(cors());
 app.use(static(__dirname + '/' + 'public'));
 
 // SOCKET.IO
-
-// let brokenSockets = {}
-
-// function activeSockets(id = null) {
-//   return Object.keys(io.sockets.connected).filter(sid => sid !== id && !brokenSockets[sid])
-// }
-
-// function brokenSocket(socket) {
-//   brokenSockets[socket.id] = true
-//   // log('--- broken sockets', Object.keys(brokenSockets).length, 'connected', activeSockets().length)
-//   io.emit('remove', { id: socket.id })
-// }
-
-// function socketByID(id) {
-//   return io.sockets.connected[id]
-// }
-
-// function emitByID(id, name, msg) {
-//   let socket = socketByID(id)
-//   if (socket) {
-//     console.log('emit', id, name, msg)
-//     socket.emit(name, msg)
-//   }
-// }
-
-// function broadcastByID(ids, name, msg) {
-//   for (let id of ids) {
-//     emitByID(id, name, msg)
-//   }
-// }
 
 io.on('connection', (socket) => {
   const sid = socket.id
